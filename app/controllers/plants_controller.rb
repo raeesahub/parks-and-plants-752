4279534b8d19ec03951b3b0c440cab88c017f4a7
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-11-10 18:10:19 +0000

    user can add a plant

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,19 @@
+class PlantsController < ApplicationController
+
+  def create
+    @plant = Plant.new(plant_params)
+    @garden = Garden.find(params[:garden_id])
+    @plant.garden = @garden
+    if @plant.save
+      redirect_to @garden
+    else
+      render 'gardens/show'
+    end
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :image_url)
+  end
+end
